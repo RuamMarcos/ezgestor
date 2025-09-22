bfba27a68dd296a73cc63ee73992e7296ab717aa
arkfx <felipe.leal@mail.uft.edu.br>
2025-09-22 12:45:30 -0300

    fix: update API base URL to point to production server

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api", // backend Django
+  //baseURL: "http://localhost:8000/api", // backend Django
+  baseURL: "https://ezgestor-fix-test-104520155285.southamerica-east1.run.app/api", // backend Django Production
 });
 
 export default api;
